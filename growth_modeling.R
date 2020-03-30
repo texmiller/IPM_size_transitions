9234709baf6e4f950795dd171c4d5f6ea3839a40
unknown <tom.miller@rice.edu>
2020-03-30 10:42:06 -0500

    New script to fit growth models to size transition data.

---
+++ growth_modeling.R
@@ -0,0 +1,32 @@
+## Model size transitions using data list prepared in dryad_data_prep.R
+library(moments)
+library(zoo)
+library(mgcv)
+
+## load data
+growth_dat <- readRDS("growth_dat.rds")
+
+## Steve's diagnostics code
+### Scatterplot smoothing functions
+spline.scatter.smooth=function(x,y,...) {
+  fit=gam(y~s(x),gamma=2,method="REML")
+  plot(x,y,type="p",...);
+  out=predict(fit,type="response"); 
+  points(x,out,type="l",lwd=2)
+  #fit2 = lm(y~x+I(x^2));
+  #points(x,fit2$fitted,type="l",col="red",lwd=2,lty=2);
+}
+
+par(mfrow=c(4,4),bty="l",mar=c(4,4,1,1),mgp=c(2,1,0),cex.axis=1.3,cex.lab=1.3);
+for(i in 1:4){
+z0 = growth_dat[[i]]$t0; z1=growth_dat[[i]]$t1; 
+rollmean<-rollapply(z0,width=10,mean,na.rm=T)
+rollmean1<-rollapply(z1,width=10,mean,na.rm=T)
+rollsd <-rollapply(z1,width=10,sd,na.rm=T); 
+rollskew<-rollapply(z1,width=10,skewness,na.rm=T)
+rollkurt<-rollapply(z1,width=10,kurtosis,na.rm=T)/3 - 1
+spline.scatter.smooth(rollmean,rollmean1,col="grey50",xlab="z0",ylab="z1");title(main=names(growth_dat[i])) 
+spline.scatter.smooth(rollmean,rollsd,col="grey50",xlab="z0",ylab="SD"); 
+spline.scatter.smooth(rollmean,rollskew,col="grey50",xlab="z0",ylab="Skew"); 
+spline.scatter.smooth(rollmean,rollkurt,col="grey50",xlab="z0",ylab="Excess Kurtosis"); 
+}
