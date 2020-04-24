8f6146b25fde92045b9a3770705a8dcb999af311
ellner <spe2@cornell.edu>
2020-04-24 14:37:09 -0400

    idaho to the end, take 1

--- idaho/ARTRgrowthModeling-log-twoStage.R
+++ idaho/ARTRgrowthModeling-log-twoStage.R
@@ -36,7 +36,7 @@ allD<-read.csv(file="ARTR_growth_data.csv");
 allD$year <- factor(allD$year); 
 
 ######################################################################
-# Tom's work starting here: exploring the idaho data
+# Tom's work: exploring the idaho data
 ######################################################################
 library(tidyverse)
 ## drop out these arbitrarily small individuals
@@ -44,10 +44,13 @@ arb_small <- unique(c(which(allD$area.t0==0.25),which(allD$area.t1==0.25)))
 drop_allD <- allD[-arb_small,]
 plot(drop_allD$logarea.t0, drop_allD$logarea.t1) 
 
-## model selection with log transformation
+########################################################################## 
+## Pilot fits with log transformation, constant variance 
+########################################################################## 
 log_models <- list()
-log_models[[1]] <- lmer(logarea.t1~logarea.t0+W.ARTR + W.HECO + W.POSE + W.PSSP+  W.allcov + W.allpts + Treatment + 
-             (1|Group)+(logarea.t0|year),control=lmerControl(optimizer="bobyqa"),data=drop_allD,REML = F) 
+vf <- varExp(form =~ logarea.t0)
+log_models[[1]] <- lmer(logarea.t1~ logarea.t0 + W.ARTR + W.HECO + W.POSE + W.PSSP+  W.allcov + W.allpts + Treatment + 
+             (1|Group)+(logarea.t0|year),control=lmerControl(optimizer="bobyqa"),data=drop_allD,REML=F); 
 
 log_models[[2]] <- lmer(logarea.t1~logarea.t0 + W.ARTR  + Treatment + 
              (1|Group)+(logarea.t0|year),control=lmerControl(optimizer="bobyqa"),data=drop_allD,REML = F)
@@ -63,26 +66,50 @@ log_models[[5]] <- lmer(logarea.t1~logarea.t0 + Treatment +
 
 log_models[[6]] <- lmer(logarea.t1~logarea.t0 + Treatment + 
              (0+logarea.t0|year),control=lmerControl(optimizer="bobyqa"),data=drop_allD,REML = F)
-			 
+
+########################################################################## 
+## Use iterative re-weighting to fit with nonconstant variance,  
+## then AIC for model selection.  
+##########################################################################
 for(mod in 1:6) {
-cat(mod,"\n"); 
-for(rep in 1:5) {
+for(rep in 1:25) {
 	log_model = log_models[[mod]];
 	fitted_vals = fitted(log_model);
 	resids = residuals(log_model); 
 	resid_model = lm(log(abs(resids))~fitted_vals); 
-	log_models[[mod]] <- update(log_model,weights=exp(-fitted(resid_model))); 
+	new_weights = 0.5*(weights(log_model) + exp(-fitted(resid_model))); # cautious update 
+	new_model <- update(log_model,weights=new_weights); 
+	err = weights(log_model)-weights(new_model); err=sqrt(mean(err^2)); 
+	cat(mod,rep,err,"\n") # check on convergence of estimated weights 
+	log_models[[mod]]<-new_model; 
 }}
 aictab(log_models); 
 
+######### For a fair AIC comparison, fit all models with the same weights 
 aics = unlist(lapply(log_models,AIC)); best_model=which(aics==min(aics)); 
 best_weights=weights(log_models[[best_model]]); 
 for(mod in 1:6) {log_models[[mod]] <- update(log_models[[mod]],weights=best_weights)}
 aictab(log_models); 
 
+########## Make sure that the model choice hasn't changed
+aics = unlist(lapply(log_models,AIC)); best_model=which(aics==min(aics)); 
+best_weights=weights(log_models[[best_model]]); 
+for(mod in 1:6) {log_models[[mod]] <- update(log_models[[mod]],weights=best_weights)}
+aictab(log_models); 
+
+######### Here's the best Gaussian model 
 aics = unlist(lapply(log_models,AIC)); best_model=which(aics==min(aics)); 
 log_model = log_models[[best_model]]; 
 
+summary(log_model); ## no Group effect, so re-fit 
+log_model <- lmer(logarea.t1~logarea.t0 + Treatment + 
+             (logarea.t0|year),control=lmerControl(optimizer="bobyqa"),data=drop_allD,
+			 weights=weights(log_model),REML = F)
+summary(log_model); 
+
+######################################################################
+# Interrogate the scaled residuals - Gaussian? NO. 
+###################################################################### 
 plot(fitted(log_model), 1/weights(log_model)); 
 log_scaledResids = residuals(log_model)*weights(log_model)
 plot(fitted(log_model), log_scaledResids); 
@@ -93,96 +120,111 @@ jarque.test(log_scaledResids) # normality test: FAILS, P < 0.001
 anscombe.test(log_scaledResids) # kurtosis: FAILS, P < 0.001 
 agostino.test(log_scaledResids) # skewness: FAILS, P<0.001 
 
+########################################################################
 ## See what family gamlss thinks the scaled residuals conform to. 
+########################################################################
 log_gamlss_fit <- fitDist(log_scaledResids,type="realline")
 log_gamlss_fit$fits
 log_gamlss_fit$failed
 
+########################################################################
+## Rolapply diagnostics on the scaled residuals 
+########################################################################
+px = fitted(log_model); py=log_scaledResids; 
+e = order(px); px=px[e]; py=py[e];  
+
+rollx=rollapply(px,50,mean,by=25);
+rollmean = rollapply(py,50,mean,by=25); 
+rollsd=rollapply(py,50,sd,by=25); 
+rollkurt=rollapply(py,50,kurtosis,by=25);
+rollskew=rollapply(py,50,skewness,by=25);
+
+par(mfrow=c(2,2),mar=c(5,5,2,1),cex.axis=1.3,cex.lab=1.3); 
+spline.scatter.smooth(rollx,rollmean,gamma=2,xlab="Fitted values",ylab="Mean");
+spline.scatter.smooth(rollx,rollsd,gamma=2,xlab="Fitted values",ylab="Std Dev"); 
+spline.scatter.smooth(rollx,rollskew,gamma=2,xlab="Fitted values",ylab="Skew"); 
+abline(h=0,col="blue",lty=2) 
+spline.scatter.smooth(rollx,rollkurt/3-1,gamma=2,xlab="Fitted values",ylab="Excess kurtosis"); 
+abline(h=0,col="blue",lty=2)
+
 ################################################################################################################
-## try fitting gamlss ST2 to log size data. use the fixed effect structure corresponding to log_models[[3]]
-## SPE: ST2 is suggested by using fitDist on the scaled residuals, and it gives a better (lower deviance) fit
+## try fitting gamlss JSU to log size data. Use the fixed effect structure corresponding to the best lmer fit. 
+## JSU is suggested by fitDist on the scaled residuals. 
 #################################################################################################################
-fit_ST2 <- gamlss(logarea.t1 ~ as.factor(year) + logarea.t0:as.factor(year) + Treatment + Group, 
-                  data=drop_allD, family="ST2", method=RS(250),
+
+#### Extract random effects from the pilot Gaussian fit, and use them as an offset 
+coefs=fixef(log_model)
+fixed.effect = coefs[1] + coefs[2]*drop_allD$logarea.t0;
+fixed.effect[drop_allD$Treatment=="ControlModern"]  =  fixed.effect[drop_allD$Treatment=="ControlModern"] + coefs[3]; 
+fixed.effect[drop_allD$Treatment=="No_grass"]  =  fixed.effect[drop_allD$Treatment=="No_grass"] + coefs[4]; 
+random.effect = fitted(log_model)-fixed.effect; 
+drop_allD$random.effect = random.effect; 
+
+### Pilot fit: variance depends on initial logarea 
+theFamily = "JSU"
+fit_LSS <- gamlss(logarea.t1 ~ logarea.t0 +  Treatment + offset(random.effect), 
+                  data=drop_allD, family=theFamily, method=RS(250), 
                   sigma.formula = ~logarea.t0, nu.formula = ~1, tau.formula = ~1)
 
+### Iterative re-fit, with variance depending on fitted values 
+drop_allD$fitted=fitted(fit_LSS);
+for(k in 1:5) {
+	fit_vals= fitted(fit_LSS);
+	drop_allD$fitted <- 0.5*(drop_allD$fitted + fit_vals); 
+	fit_LSS <- gamlss(logarea.t1 ~ logarea.t0 + Treatment + offset(random.effect),
+                  data=drop_allD, family= theFamily, method=RS(250),start.from=fit_LSS,
+                  sigma.formula = ~fitted, nu.formula = ~1, tau.formula = ~1)
+	new_fit = fitted(fit_LSS); 
+	err = (new_fit - fit_vals)^2; 
+	cat(k, mean(err)^0.5,"\n"); 
+}				  
+
+###############################################################################
 ## well, it fit. does it describe the data well?
-## simulate data from best model
-n_sim <- 500
+## Simulate data from best model
+###############################################################################
+n_sim <- 100
 idaho_sim<-matrix(NA,nrow=nrow(drop_allD),ncol=n_sim)
 for(i in 1:n_sim){
-  idaho_sim[,i] <- rST2(n = nrow(drop_allD), 
-                        mu = predict(fit_ST2), 
-                        sigma = exp(fit_ST2$sigma.coefficients[1] + fit_ST2$sigma.coefficients[2] * drop_allD$logarea.t0),
-                        nu = exp(fit_ST2$nu.coefficients),
-                        tau = exp(fit_ST2$tau.coefficients))
+  idaho_sim[,i] <- rJSU(n = nrow(drop_allD), 
+                        mu = predict(fit_LSS), sigma = fit_LSS$sigma.fv,
+                        nu = fit_LSS$nu.fv, tau = fit_LSS$tau.fv)
 }
 
+################################################################
+#  Rollapply diagnostics applied to the fitted model 
+#  Compare mean, sd, skewness, excess kurtosis vs. fitted value
+################################################################
+px = fitted(fit_ST2); e = order(px); px=px[e]; 
 
-## moments of the real data by size bin
-n_bins = 10
-alpha_scale = 0.7
-idaho_moments <- drop_allD %>% 
-  arrange(logarea.t0) %>% 
-  mutate(size_bin = cut_number(logarea.t0,n=n_bins)) %>% 
-  group_by(size_bin) %>% 
-  summarise(mean_t1 = mean(logarea.t1),
-            sd_t1 = sd(logarea.t1),
-            skew_t1 = skewness(logarea.t1),
-            kurt_t1 = kurtosis(logarea.t1),
-            bin_mean = mean(logarea.t0),
-            bin_n = n()) 
+## Real data 
+py = drop_allD$logarea.t1; py=py[e]; 
+rollx=rollapply(px,100,mean,by=50);
+rollmean = rollapply(py,100,mean,by=50); 
+rollsd=rollapply(py,100,sd,by=50); 
+rollskew=rollapply(py,100,skewness,by=50);
+rollkurt=rollapply(py,100,kurtosis,by=50)/3-1;
 
-par(mfrow=c(2,2))
-plot(idaho_moments$bin_mean,idaho_moments$mean_t1,type="n",xlab="Mean size t0",ylab="mean(Size t1)",ylim=c(1,8))
-for(i in 1:n_sim){
-  sim_moments <- bind_cols(drop_allD,data.frame(sim=idaho_sim[,i])) %>% 
-    arrange(logarea.t0) %>% 
-    mutate(size_bin = cut_number(logarea.t0,n=n_bins)) %>% 
-    group_by(size_bin) %>% 
-    summarise(mean_t1 = mean(sim),
-              bin_mean = mean(logarea.t0))
-  points(sim_moments$bin_mean,sim_moments$mean_t1,col=alpha("gray",0.5))
+thePlot=function(x,Y,xlab,ylab,gamma=2){
+    pY= matrix(0,nrow(Y),ncol(Y))
+	for(j in 1:ncol(Y)) {
+	  fit=gam(Y[,j]~s(x),gamma=gamma,method="REML")
+      pY[,j]=predict(fit,type="response"); 
+    } 
+	matplot(x,pY,col="grey50",type="o",pch=1,xlab=xlab,ylab=ylab);	
+	points(x,pY[,1],col="red",cex=1.5,pch=16); 
 }
-points(idaho_moments$bin_mean,idaho_moments$mean_t1,cex=2,pch=16,col=alpha("red",alpha_scale))
 
-plot(idaho_moments$bin_mean,idaho_moments$sd_t1,type="n",xlab="Mean size t0",ylab="sd(Size t1)",ylim=c(0,4))
-for(i in 1:n_sim){
-  sim_moments <- bind_cols(drop_allD,data.frame(sim=idaho_sim[,i])) %>% 
-    arrange(logarea.t0) %>% 
-    mutate(size_bin = cut_number(logarea.t0,n=n_bins)) %>% 
-    group_by(size_bin) %>% 
-    summarise(sd_t1 = sd(sim),
-              bin_mean = mean(logarea.t0))
-  points(sim_moments$bin_mean,sim_moments$sd_t1,col=alpha("gray",0.5))
-}
-points(idaho_moments$bin_mean,idaho_moments$sd_t1,cex=2,pch=16,col=alpha("red",alpha_scale))
+par(mfrow=c(2,2),mar=c(5,5,2,1),cex.axis=1.3,cex.lab=1.3); 
 
-plot(idaho_moments$bin_mean,idaho_moments$skew_t1,type="n",xlab="Mean size t0",ylab="skewness(Size t1)",ylim=c(-10,1))
-abline(h=0,col="blue",lty=2) 
-for(i in 1:n_sim){
-  sim_moments <- bind_cols(drop_allD,data.frame(sim=idaho_sim[,i])) %>% 
-    arrange(logarea.t0) %>% 
-    mutate(size_bin = cut_number(logarea.t0,n=n_bins)) %>% 
-    group_by(size_bin) %>% 
-    summarise(skew_t1 = skewness(sim),
-              bin_mean = mean(logarea.t0))
-  points(sim_moments$bin_mean,sim_moments$skew_t1,col=alpha("gray",0.5))
-}
-points(idaho_moments$bin_mean,idaho_moments$skew_t1,cex=2,pch=16,col=alpha("red",alpha_scale))
+for(j in 1:n_sim) Y[,j] = rollapply(idaho_sim[e,j],100,mean,by=50); 
+thePlot(rollx,cbind(rollmean,Y),xlab="Fitted value",ylab="Mean");
 
-plot(idaho_moments$bin_mean,idaho_moments$kurt_t1,type="n",xlab="Mean size t0",ylab="kurtosis(Size t1)",ylim=c(0,25))
-abline(h=0,col="blue",lty=2) 
-for(i in 1:n_sim){
-  sim_moments <- bind_cols(drop_allD,data.frame(sim=idaho_sim[,i])) %>% 
-    arrange(logarea.t0) %>% 
-    mutate(size_bin = cut_number(logarea.t0,n=n_bins)) %>% 
-    group_by(size_bin) %>% 
-    summarise(kurt_t1 = kurtosis(sim),
-              bin_mean = mean(logarea.t0))
-  points(sim_moments$bin_mean,sim_moments$kurt_t1,col=alpha("gray",0.5))
-}
-points(idaho_moments$bin_mean,idaho_moments$kurt_t1,cex=2,pch=16,col=alpha("red",alpha_scale))
+for(j in 1:n_sim) Y[,j] = rollapply(idaho_sim[e,j],100,sd,by=50); 
+thePlot(rollx,cbind(rollsd,Y),xlab="Fitted value",ylab="Std Dev");
 
+for(j in 1:n_sim) Y[,j] = rollapply(idaho_sim[e,j],100,skewness,by=50); 
+thePlot(rollx,cbind(rollskew,Y),xlab="Fitted value",ylab="Skewness");
 
-
+for(j in 1:n_sim) Y[,j] = rollapply(idaho_sim[e,j],100,kurtosis,by=50)/3-1; 
+thePlot(rollx,cbind(rollkurt,Y),xlab="Fitted value",ylab="Excess Kurtosis");
