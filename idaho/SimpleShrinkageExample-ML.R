83b9e6e38974244b21e9881b26859b1c0dd2d101
ellner <spe2@cornell.edu>
2020-05-15 13:43:50 -0400

    shrinkage still

--- idaho/SimpleShrinkageExample-ML.R
+++ idaho/SimpleShrinkageExample-ML.R
@@ -11,8 +11,6 @@ setwd("c:/repos/IPM_size_transitions/idaho");
 require(lme4); require(tidyverse); require(maxLik); 
 source("Utilities.R");
 
-set.seed(1774); 
-
 ##############################################################
 # 1. Generate artificial data for the example 
 ##############################################################
@@ -22,67 +20,59 @@ allD<-read.csv(file="PSSP_growth_data.csv");
 allD$year <- factor(allD$year); 
 
 ## drop out these arbitrarily small individuals
-e = which(abs(allD$area.t0-0.25)<0.001); 
-dropD = allD[-e,]; 
-e = which(abs(dropD$area.t1-0.25)<0.001); 
-dropD = dropD[-e,]; 
+e1 = which(abs(allD$area.t0-0.25)<0.001); 
+e2 = which(abs(allD$area.t1-0.25)<0.001); 
+dropD = allD[-c(e1,e2),]; 
 
-# Limit to control quadrats, historical and modern
-e = which(dropD$Treatment=="ControlModern");
-dropD$Treatment[e] = "Control"; 
-dropD = subset(dropD,Treatment=="Control"); 
-dropD = droplevels(dropD);  
+# Limit to historical control quadrats
+e = which(dropD$Treatment=="Control");
+dropD = droplevels(dropD[e,]);  
 plot(dropD$logarea.t0, dropD$logarea.t1) 
 
-
-## Fit with nonconstant variance
-log_model <- lmer(logarea.t1 ~ logarea.t0 + (logarea.t0|year),
-			control=lmerControl(optimizer="bobyqa"),data=dropD,REML=TRUE); 
-
-varPars = function(pars) {
-    return(-sum(dnorm(resids, mean=0, sd=exp(pars[1] + pars[2]*fitted_vals),log=TRUE)))
-}	
-
-err = 1; rep=0; while(err > 0.000001) {
-	rep=rep+1; 
-	fitted_vals = fitted(log_model); resids = residuals(log_model); 
-	out=optim(c(sd(resids),0),varPars,method="BFGS",control=list(maxit=5000)); 
-	pars=out$par; 
-	new_sigma = exp(pars[1] + pars[2]*fitted_vals); new_weights = 1/((new_sigma)^2)
-	new_weights = 0.5*(weights(log_model) + new_weights);  
-	new_model <- update(log_model,weights=new_weights); 
-	err = weights(log_model)-weights(new_model); err=sqrt(mean(err^2)); 
-	cat(rep,err,"\n") # check on convergence of estimated weights 
-	log_model <- new_model; 
+################# Fit an SN1 distribution by maxLik 
+# Note, sigma depends on fitted value. Sigma depending on initial
+# size was much worse (Delta AIC of about 30)
+LogLik=function(pars,new.size,U){
+	pars1 = pars[1:ncol(U)]; pars2=pars[-(1:ncol(U))];
+	mu = U%*%pars1;  
+	dSN1(new.size, mu=mu, sigma=exp(pars2[1]+pars2[2]*mu),nu=pars2[3],log=TRUE)
 }
+U=model.matrix(~year + logarea.t0:year - 1, data=dropD);
+start=c(runif(ncol(U)), rep(0,3))
+out=maxLik(logLik=LogLik,start=start, new.size=dropD$logarea.t1,U=U,
+			method="BHHH",control=list(iterlim=5000,printLevel=2),finalHessian=FALSE); 
+out=maxLik(logLik=LogLik,start=out$estimate, new.size=dropD$logarea.t1,U=U,
+			method="NM",control=list(iterlim=10000,printLevel=1),finalHessian=FALSE); 
+out=maxLik(logLik=LogLik,start=out$estimate, new.size=dropD$logarea.t1,U=U,
+			method="BHHH",control=list(iterlim=5000,printLevel=2),finalHessian=FALSE); 
+pars=out$estimate; 
 
-e  = sample(1:nrow(dropD),3000,replace=FALSE)
-simData = data.frame(init.size = dropD$logarea.t0[e], year = dropD$year[e]);
-simData$year=factor(simData$year); 
-mu = fitted(log_model)[e]; 
-simData$new.size = rnorm(3000,mean=mu,sd=exp(pars[1] + pars[2]*mu));
+simData = data.frame(init.size = dropD$logarea.t0, year = factor(dropD$year));
+pars1 = pars[1:ncol(U)]; pars2=pars[-(1:ncol(U))];
+mu = U%*%pars1; nu=pars2[3]
+simData$new.size = rSN1(nrow(simData),mu=mu,sigma=exp(pars2[1]+pars2[2]*mu),nu=pars2[3]);
 
 plot(new.size~init.size,data=simData); 
+truePars = pars; 
+trueRanIntercept = truePars[1:22]-mean(truePars[1:22])
+trueRanSlope = truePars[23:44]-mean(truePars[23:44])
 
 ##############################################################
 # 2. Fit in a fixed-effects framework 
 ##############################################################
-
-### Log Likelihood function as required by maxLik 
 LogLik=function(pars,new.size,U){
 	pars1 = pars[1:ncol(U)]; pars2=pars[-(1:ncol(U))];
 	mu = U%*%pars1;  
-	dnorm(new.size, mean=mu, sd=exp(pars2[1]+pars2[2]*mu),log=TRUE)
+	dSN1(new.size, mu=mu, sigma=exp(pars2[1]+pars2[2]*mu),nu=pars2[3],log=TRUE)
 }
 
 U=model.matrix(~year + init.size:year - 1, data=simData);
-
-start=c(runif(ncol(U)), rep(0,2))
+start=c(runif(ncol(U)), rep(0,3))
 out=maxLik(logLik=LogLik,start=start, new.size=simData$new.size,U=U,
 			method="BHHH",control=list(iterlim=5000,printLevel=2),finalHessian=FALSE); 
 out=maxLik(logLik=LogLik,start=out$estimate, new.size=simData$new.size,U=U,
 			method="NM",control=list(iterlim=10000,printLevel=2),finalHessian=FALSE); 
-out=maxLik(logLik=LogLik,start=start, new.size=simData$new.size,U=U,
+out=maxLik(logLik=LogLik,start=out$estimate, new.size=simData$new.size,U=U,
 			method="BHHH",control=list(iterlim=5000,printLevel=2),finalHessian=TRUE); 
 
 #####save results of ML fit  
@@ -92,18 +82,19 @@ coefs = out$estimate; SEs = sqrt(diag(vcov(out))); names(coefs)<-colnames(U);
 # 3. Shrink the fixed-effects estimates 
 ##############################################################
 
-fixed.fx = coefs[1:30]; fixed.fx = fixed.fx-mean(fixed.fx); 
-fixed.se = SEs[1:30]; 
+fixed.fx = coefs[1:22]; fixed.fx = fixed.fx-mean(fixed.fx); 
+fixed.se = SEs[1:22]; 
 var.hat = mean(fixed.fx^2)-mean(fixed.se^2)
 shrinkRanIntercept = fixed.fx*sqrt(var.hat/(var.hat + fixed.se^2));
 
-fixed.fx2 = coefs[31:60]; fixed.fx2 = fixed.fx2-mean(fixed.fx2); 
-fixed.se2 = SEs[31:60]; 
+fixed.fx2 = coefs[23:44]; fixed.fx2 = fixed.fx2-mean(fixed.fx2); 
+fixed.se2 = SEs[23:44]; 
 var2.hat = mean(fixed.fx2^2)-mean(fixed.se2^2)
 shrinkRanSlope = fixed.fx2*sqrt(var2.hat/(var2.hat + fixed.se2^2)); 
 
 graphics.off(); dev.new(height=4,width=8)
 par(mfrow=c(1,2),bty="l",mar=c(4,4,1,1),mgp=c(2,1,0)); 
-plot(ranef(log_model)$year[,1],shrinkRanIntercept,type="p",xlab="True year-specific intercept",ylab="Estimated"); abline(0,1); 
-plot(ranef(log_model)$year[,2],shrinkRanSlope,type="p",xlab="True year-specific slope",ylab="Estimated"); abline(0,1); 
+plot(trueRanIntercept,shrinkRanIntercept,type="p",xlab="True year-specific intercept",ylab="Estimated"); abline(0,1); 
+plot(trueRanSlope,shrinkRanSlope,type="p",xlab="True year-specific slope",ylab="Estimated"); abline(0,1); 
 
+save.image(file="SimpleShrinkageExample.Rdata"); 
