d6e165c8544bde3b18099975e3ccb3ced8c13061
Thomas E Miller <tm9@rice.edu>
2020-09-11 14:22:01 -0500

    Added random effect to gam practice.

--- gam_practice.R
+++ gam_practice.R
@@ -1,20 +1,30 @@
+library(mgcv)
+library(maxLik)
 
-  x1 = runif(1000,-2,2)
-  x2 = runif(1000,4,15)
-  mu = 4.5 + 2.3*x1 + 0.7*x1^2 - 0.09*x1^3
-  y = rnorm(1000,mu,2)
-
-plot(x1,y)
-testgam <- gam(y~s(x1),data=data.frame(y=y,x1=x1))
-pred <- predict.gam(testgam,newdata = data.frame(x1=seq(-2,2,0.01)))
-lines(seq(-2,2,0.01),pred,col="red",lwd=3)
+n_obs<-1000
+n_levels<-10
+x1 = runif(n_obs,-2,2)
+rfx<-rnorm(n_levels,4.5,3)
+mu = rep(rfx,(n_obs/n_levels)) + 2.3*x1 - 0.5*x1^2 + 1.9*x1^3
+y = rnorm(1000,mu,2)
+plot(x1,y,col="gray")
+practice.dat <- data.frame(x1=x1,
+                           rfx=as.factor(rep(letters[1:n_levels],(n_obs/n_levels))),
+                           y=y)
+testgam <- gam(y~s(x1)+s(rfx,bs="re"),data=practice.dat)
+## prediction ignoring random effect
+pred <- predict.gam(testgam,
+                    newdata = data.frame(x1=seq(-2,2,0.01),
+                                         rfx="foo"), 
+                    exclude = "s(rfx)")
+lines(seq(-2,2,0.01),pred,col="red",lwd=5)
 
 # coefficients
 beta <- testgam$coefficients
 # linear predictor
 Xp <- predict.gam(testgam,type = "lpmatrix")
 
-plot(x1,Xp%*%beta)
+plot(x1,Xp%*%beta) ## no the rfx levels apparent here
 
 ## can I "re-fit" this spline by ML using the design matrix?
 LogLik=function(pars,response,U){
@@ -38,5 +48,10 @@ plot(beta,practice.out$estimate[1:ncol(Xp)])
 abline(0,1)
 
 plot(x1,y,col="lightgray")
-points(x1,Xp%*%practice.out$estimate[1:ncol(Xp)],pch=16,col="blue")
-lines(seq(-2,2,0.01),pred,col="red",lwd=3)
+points(x1,Xp%*%practice.out$estimate[1:ncol(Xp)],pch=".",col="blue")
+lines(seq(-2,2,0.01),pred,col="red",lwd=6)
+## I can pull out the average across rfx levels by setting those coefficients to zero
+beta_mean <- practice.out$estimate
+beta_mean[colnames(Xp) %in% paste0("s(rfx).",1:n_levels)] <- 0
+points(x1,Xp%*%beta_mean[1:ncol(Xp)],col="blue")
+# WORKS
