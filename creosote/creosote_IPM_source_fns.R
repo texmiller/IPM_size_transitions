1a2eb93d0ea78f263219aaa0fbd9e65e88d3b9c2
Thomas E Miller <tm9@rice.edu>
2020-11-23 22:51:10 -0600

    Updated with gaussian growth function and fruit function. Need to figure out how to implement non-Gaussian growth function -- can't use predict.gam()

--- creosote/creosote_IPM_source_fns.R
+++ creosote/creosote_IPM_source_fns.R
@@ -2,27 +2,40 @@
 ## These functions are size (x) and density (d), with no other spatial or temporal random effects
 ## d is the weighted density of the window (sum of log volume)
 
-## Growth
-#growth_fn <- 
+## Growth -- Gaussian using best gam
+growth_fn_norm <- function(x,y,d){
+  lpmat <- predict.gam(LATR_gam_model,
+                      newdata = data.frame(
+                        weighted.dens = d,
+                        log_volume_t = x,
+                        unique.transect="foo"),
+                      type="lpmatrix",
+                      exclude = "s(unique.transect)")
+  ## linear predictor for mean and log sigma -- need to update so these indices are not hard-coded but for now they work
+  grow_mu <- lpmat[,1:19]%*%coef(LATR_gam_model)[1:19]
+  grow_sigma <- exp(lpmat[,32:50]%*%coef(LATR_gam_model)[32:50])
+  return(dnorm(y,mean=grow_mu,sd=grow_sigma))
+}
+growth_fn_norm(5,5,0)
 
 ## Survival-- prediction from naturally occuring plants
 survival_fn <- function(x,d){
   pred <- predict.gam(LATR_surv_best,
               newdata = data.frame(
-                d.stand = d,
+                weighted.dens = d,
                 log_volume_t = x,
                 transplant=F,
                 unique.transect="foo"),
               exclude = "s(unique.transect)")
   return(invlogit(pred))
 }
-survival_fn(x=5,d=0)
+survival_fn(x=0,d=0)
 
 ## Flowering
 flower_fn <- function(x,d){
   pred <- predict.gam(LATR_flower_best,
               newdata = data.frame(
-                d.stand = d,
+                weighted.dens = d,
                 log_volume_t = x,
                 unique.transect="foo"),
               exclude = "s(unique.transect)")
@@ -30,3 +43,15 @@ flower_fn <- function(x,d){
 }
 
 flower_fn(x=5,d=0)
+
+## Fruits
+fruits_fn <- function(x,d){
+  pred <- predict.gam(LATR_fruits_best,
+                      newdata = data.frame(
+                        weighted.dens = d,
+                        log_volume_t = x,
+                        unique.transect="foo"),
+                      exclude = "s(unique.transect)")
+  return(exp(pred))
+}
+fruits_fn(x=5,d=0)
