c06de5b608d113f76ce013e3394416f1418b9154
Tom Miller <tom.miller@rice.edu>
2021-07-29 16:25:14 -0500

    Update creosote_growth_sgt.R
    added additional vital rate data and fits to start assembling the full IPM

--- creosote/creosote_growth_sgt.R
+++ creosote/creosote_growth_sgt.R
@@ -284,3 +284,296 @@ points(LATR_moments$bin_mean, apply(sim_moment_means,1,median),pch=1,lwd=2,col=a
 points(LATR_moments$bin_mean+0.4, apply(sim_moment_means_norm,1,median),pch=1,lwd=2,col=alpha("black",alpha_scale),cex=1.6)
 add_panel_label("d")
 
+
+# IPM ---------------------------------------------------------------------
+# Flowering ---------------------------------------------------------------
+# populate year t of 2017-2018 transition year (there are no 2018 data but this way we get all four years in the reproduction models)
+# I'll do this by creating the 2017-18 data as a stand-alone df then bind rows
+LATR_dat_201718 <- LATR_full[LATR_full$year_t==2016 & LATR_full$survival_t1==1,]
+## these are the 2017 survivors. Make their year t demography last year's data
+LATR_dat_201718$year_t<-2017;LATR_dat_201718$year_t1<-2018
+LATR_dat_201718$max.ht_t<-LATR_dat_201718$max.ht_t1
+LATR_dat_201718$max.w_t<-LATR_dat_201718$max.w_t1
+LATR_dat_201718$volume_t<-LATR_dat_201718$volume_t1
+LATR_dat_201718$perp.w_t<-LATR_dat_201718$perp.w_t1
+LATR_dat_201718$flowers_t<-LATR_dat_201718$flowers_t1
+LATR_dat_201718$fruits_t<-LATR_dat_201718$fruits_t1
+LATR_dat_201718$reproductive_fraction_t<-LATR_dat_201718$reproductive_fraction_t1
+LATR_dat_201718$total.reproduction_t<-LATR_dat_201718$total.reproduction_t1
+## now set all the t1 data to NA
+LATR_dat_201718$max.ht_t1<-NA
+LATR_dat_201718$max.w_t1<-NA
+LATR_dat_201718$volume_t1<-NA
+LATR_dat_201718$perp.w_t1<-NA
+LATR_dat_201718$flowers_t1<-NA
+LATR_dat_201718$fruits_t1<-NA
+LATR_dat_201718$reproductive_fraction_t1<-NA
+LATR_dat_201718$total.reproduction_t1<-NA
+## bind rows
+LATR_flow_dat <- bind_rows(LATR_full,LATR_dat_201718) %>% 
+  select(unique.transect,volume_t,total.reproduction_t,weighted.dens) %>% drop_na()
+## create log_vol as new variables (easier for gams)
+LATR_flow_dat$log_volume_t <- log(LATR_flow_dat$volume_t)
+
+LATR_flower <- list()
+LATR_flower[[1]] <-  gam(total.reproduction_t>0 ~ s(log_volume_t) + s(unique.transect,bs="re"),
+                         data=LATR_flow_dat, gamma=1.4, family="binomial")
+LATR_flower[[2]] <-  gam(total.reproduction_t>0 ~ s(log_volume_t) + s(weighted.dens) + s(unique.transect,bs="re"),
+                         data=LATR_flow_dat, gamma=1.4, family="binomial")
+LATR_flower[[3]] <-  gam(total.reproduction_t>0 ~ s(log_volume_t) + s(weighted.dens) + weighted.dens:log_volume_t  + s(unique.transect,bs="re"),
+                         data=LATR_flow_dat, gamma=1.4, family="binomial")
+flower_aic<-AICtab(LATR_flower,base=T,sort=F)
+LATR_flower_best <- LATR_flower[[which.min(flower_aic$AIC)]]
+saveRDS(LATR_flower_best,"LATR_flower_best.rds")
+LATR_flower_fitted_terms = predict(LATR_flower_best,type="terms") 
+LATR_flow_dat$pred = predict.gam(LATR_flower_best,newdata = LATR_flow_dat, exclude = "s(unique.transect)")
+
+##### effect of size on pr(flower) -- fairly linear
+plot(LATR_flow_dat$log_volume_t,LATR_flower_fitted_terms[,"s(log_volume_t)"]) 
+#### effect of d.stand on pr(flower) -- linear 
+plot(LATR_flow_dat$weighted.dens,LATR_flower_fitted_terms[,"s(weighted.dens)"]) 
+
+## visualize data + model
+n_cuts_dens <- 6
+n_cuts_size <- 4
+LATR_flow_dat %>% 
+  mutate(size_bin = as.integer(cut_number(log_volume_t,n_cuts_size)),
+         dens_bin = as.integer(cut_number(weighted.dens,n_cuts_dens))) %>% 
+  group_by(size_bin,dens_bin) %>% 
+  summarise(mean_size = mean(log_volume_t),
+            mean_density = mean(weighted.dens),
+            mean_flower = mean(total.reproduction_t > 0),
+            pred_flower = mean(pred),
+            bin_n = n()) -> LATR_flow_dat_plot
+
+## generate predictions for plotting
+size_means_flow <- LATR_flow_dat_plot %>% group_by(size_bin) %>% summarise(mean_size=mean(mean_size))
+LATR_flow_pred <- data.frame(
+  weighted.dens = rep(seq(min(LATR_flow_dat$weighted.dens),max(LATR_flow_dat$weighted.dens),length.out = 20),times=n_cuts_size),
+  log_volume_t = rep(size_means_flow$mean_size,each=20),
+  unique.transect=1,
+  size_bin = rep(size_means_flow$size_bin,each=20)
+)
+LATR_flow_pred$pred <- predict.gam(LATR_flower_best,newdata = LATR_flow_pred, exclude = "s(unique.transect)")
+
+plot(LATR_flow_dat_plot$mean_density,LATR_flow_dat_plot$mean_flower,type="n",ylim=c(0,1),
+     xlab="Weighted density",ylab="Pr(Flowering)")
+for(i in 1:n_cuts_size){
+  points(LATR_flow_dat_plot$mean_density[LATR_flow_dat_plot$size_bin==i],
+         LATR_flow_dat_plot$mean_flower[LATR_flow_dat_plot$size_bin==i],pch=16,col=i,
+         cex=(LATR_flow_dat_plot$bin_n[LATR_flow_dat_plot$size_bin==i]/max(LATR_flow_dat_plot$bin_n))*3)
+  lines(LATR_flow_pred$weighted.dens[LATR_flow_pred$size_bin==i],
+        invlogit(LATR_flow_pred$pred[LATR_flow_pred$size_bin==i]),col=i)
+}
+
+
+# Fruit production --------------------------------------------------------
+LATR_fruits_dat <- subset(LATR_flow_dat,total.reproduction_t>0)
+LATR_fruits <- list()
+LATR_fruits[[1]] <-  gam(total.reproduction_t ~ s(log_volume_t) + s(unique.transect,bs="re"),
+                         data=LATR_fruits_dat, gamma=1.4, family="nb")
+LATR_fruits[[2]] <-  gam(total.reproduction_t ~ s(log_volume_t) + s(weighted.dens) + s(unique.transect,bs="re"),
+                         data=LATR_fruits_dat, gamma=1.4, family="nb")
+LATR_fruits[[3]] <-  gam(total.reproduction_t ~ s(log_volume_t) + s(weighted.dens) + weighted.dens:log_volume_t  + s(unique.transect,bs="re"),
+                         data=LATR_fruits_dat, gamma=1.4, family="nb")
+fruits_aic<-AICtab(LATR_fruits,base=T,sort=F)
+LATR_fruits_best <- LATR_fruits[[which.min(fruits_aic$AIC)]]
+saveRDS(LATR_fruits_best,"LATR_fruits_best.rds")
+LATR_fruits_fitted_terms = predict(LATR_fruits_best,type="terms") 
+LATR_fruits_dat$pred = predict.gam(LATR_fruits_best,newdata = LATR_fruits_dat,exclude="s(unique.transect)")
+
+##### effect of size on pr(flower) -- linear, positive
+plot(LATR_fruits_dat$log_volume_t,LATR_fruits_fitted_terms[,"s(log_volume_t)"]) 
+#### effect of d.stand on pr(flower) -- negative, a bit of a kink
+plot(LATR_fruits_dat$weighted.dens,LATR_fruits_fitted_terms[,"s(weighted.dens)"]) 
+
+LATR_fruits_dat %>% 
+  mutate(size_bin = as.integer(cut_number(log_volume_t,n_cuts_size)),
+         dens_bin = as.integer(cut_number(weighted.dens,n_cuts_dens))) %>% 
+  group_by(size_bin,dens_bin) %>% 
+  summarise(mean_size = mean(log_volume_t),
+            mean_density = mean(weighted.dens),
+            mean_fruits = mean(total.reproduction_t),
+            pred_fruits = mean(pred),
+            bin_n = n()) -> LATR_fruits_dat_plot
+
+## new data set for gam prediction
+size_means_fruit <- LATR_fruits_dat_plot %>% group_by(size_bin) %>% summarise(mean_size=mean(mean_size))
+LATR_fruit_pred <- data.frame(
+  weighted.dens = rep(seq(min(LATR_fruits_dat$weighted.dens),max(LATR_fruits_dat$weighted.dens),length.out = 20),times=n_cuts_size),
+  log_volume_t = rep(size_means_fruit$mean_size,each=20),
+  unique.transect=1,
+  size_bin = rep(size_means_fruit$size_bin,each=20)
+)
+LATR_fruit_pred$pred <- predict.gam(LATR_fruits_best,newdata = LATR_fruit_pred, exclude = "s(unique.transect)")
+
+
+plot(LATR_fruits_dat_plot$mean_density,LATR_fruits_dat_plot$mean_fruits,type="n",
+     xlab="Weighted density",ylab="Flowers and Fruits")
+for(i in 1:n_cuts_size){
+  points(LATR_fruits_dat_plot$mean_density[LATR_fruits_dat_plot$size_bin==i],
+         LATR_fruits_dat_plot$mean_fruits[LATR_fruits_dat_plot$size_bin==i],pch=16,col=i,
+         cex=(LATR_fruits_dat_plot$bin_n[LATR_fruits_dat_plot$size_bin==i]/max(LATR_fruits_dat_plot$bin_n))*3)
+  lines(LATR_fruit_pred$weighted.dens[LATR_fruit_pred$size_bin==i],
+        exp(LATR_fruit_pred$pred[LATR_fruit_pred$size_bin==i]),col=i)
+}
+
+
+# Survival ----------------------------------------------------------------
+# read in transplant experiment and merge with obs data
+# Combine transplants with large shrubs for later survival analysis
+# Keep only location info, survival, volume, and density
+CData.Transplants<-read.csv("CData.Transplants.csv")%>% 
+  select("site", "transect", "actual.window", 
+         "spring_survival_t1", "volume_t", "weighted.dens", "transplant") %>% 
+  rename("survival_t1" = "spring_survival_t1") %>% 
+  mutate(unique.transect = interaction(transect, site)) %>% 
+  rbind(select(LATR_full, "site", "transect", "actual.window", 
+               "survival_t1", "volume_t", "weighted.dens", "transplant","unique.transect")) %>% 
+  mutate(log_volume_t = log(volume_t)) %>% 
+  drop_na()-> LATR_surv_dat
+
+## how much size overlap do we have between transplant experiment and observational census?
+hist(log(LATR_surv_dat$volume_t[LATR_surv_dat$transplant==F]))
+hist(log(LATR_surv_dat$volume_t[LATR_surv_dat$transplant==T]),add=T,col=alpha("gray",0.5))
+
+plot(log(LATR_surv_dat$volume_t[LATR_surv_dat$transplant==F]),
+     LATR_surv_dat$survival_t1[LATR_surv_dat$transplant==F])
+points(log(LATR_surv_dat$volume_t[LATR_surv_dat$transplant==T]),
+       LATR_surv_dat$survival_t1[LATR_surv_dat$transplant==T]-0.025,pch=2)
+
+LATR_surv <- list()
+LATR_surv[[1]] <-  gam(survival_t1 ~ s(log_volume_t) + transplant + s(unique.transect,bs="re"),
+                       data=LATR_surv_dat, gamma=1.4, family="binomial")
+LATR_surv[[2]] <-  gam(survival_t1 ~ s(log_volume_t) + s(weighted.dens)  + transplant + s(unique.transect,bs="re"),
+                       data=LATR_surv_dat, gamma=1.4, family="binomial")
+LATR_surv[[3]] <-  gam(survival_t1 ~ s(log_volume_t) + s(weighted.dens) + transplant + weighted.dens:log_volume_t + s(unique.transect,bs="re"),
+                       data=LATR_surv_dat, gamma=1.4, family="binomial")
+surv_aic<-AICtab(LATR_surv,base=T,sort=F)
+LATR_surv_best <- LATR_surv[[which.min(surv_aic$AIC)]]
+saveRDS(LATR_surv_best,"LATR_surv_best.rds")
+LATR_surv_fitted_terms = predict(LATR_surv_best,type="terms") 
+LATR_surv_dat$pred = predict.gam(LATR_surv_best,newdata = LATR_surv_dat,exclude="s(unique.transect)")
+
+##### effect of size on pr(survival) -- linear, positive
+plot(LATR_surv_dat$log_volume_t,LATR_surv_fitted_terms[,"s(log_volume_t)"]) 
+#### effect of d.stand on pr(survival) -- negativ, a bit of a kink
+plot(LATR_surv_dat$weighted.dens,LATR_surv_fitted_terms[,"s(weighted.dens)"]) 
+
+## visualize data + model -- this is for the natural census
+n_cuts_dens <- 4
+n_cuts_size <- 4
+LATR_surv_dat %>% 
+  filter(transplant==F) %>% 
+  mutate(size_bin = as.integer(cut_interval(log_volume_t,n_cuts_size)),
+         dens_bin = as.integer(cut_interval(weighted.dens,n_cuts_dens))) %>% 
+  group_by(size_bin,dens_bin) %>% 
+  summarise(mean_size = mean(log_volume_t),
+            mean_density = mean(weighted.dens),
+            mean_surv = mean(survival_t1),
+            pred_surv = mean(pred),
+            bin_n = n()) -> LATR_surv_nat_plot
+
+## generate predictions for plotting
+size_means_surv_nat <- LATR_surv_nat_plot %>% group_by(size_bin) %>% summarise(mean_size=mean(mean_size))
+LATR_surv_nat_pred <- data.frame(
+  weighted.dens = rep(seq(min(LATR_surv_nat_plot$mean_density),max(LATR_surv_nat_plot$mean_density),length.out = 20),times=n_cuts_size),
+  log_volume_t = rep(size_means_surv_nat$mean_size,each=20),
+  unique.transect=1,
+  transplant=F,
+  size_bin = rep(size_means_surv_nat$size_bin,each=20)
+)
+LATR_surv_nat_pred$pred <- predict.gam(LATR_surv_best,newdata = LATR_surv_nat_pred, exclude = "s(unique.transect)")
+
+plot(LATR_surv_nat_plot$mean_density,LATR_surv_nat_plot$mean_surv,type="n",ylim=c(0,1),
+     xlab="Weighted density",ylab="Pr(Survival)")
+for(i in 1:n_cuts_size){
+  points(LATR_surv_nat_plot$mean_density[LATR_surv_nat_plot$size_bin==i],
+         LATR_surv_nat_plot$mean_surv[LATR_surv_nat_plot$size_bin==i],pch=16,col=i,
+         cex=(LATR_surv_nat_plot$bin_n[LATR_surv_nat_plot$size_bin==i]/max(LATR_surv_nat_plot$bin_n))*3)
+  lines(LATR_surv_nat_pred$weighted.dens[LATR_surv_nat_pred$size_bin==i],
+        invlogit(LATR_surv_nat_pred$pred[LATR_surv_nat_pred$size_bin==i]),col=i)
+}
+
+## now transplants
+LATR_surv_dat %>% 
+  filter(transplant==T) %>% 
+  mutate(dens_bin = as.integer(cut_interval(weighted.dens,n_cuts_dens)),
+         mean_size = mean(log_volume_t)) %>% 
+  group_by(dens_bin) %>% 
+  summarise(mean_size = unique(mean_size),
+            mean_density = mean(weighted.dens),
+            mean_surv = mean(survival_t1),
+            bin_n = n()) -> LATR_surv_exp_plot
+LATR_surv_exp_pred <- data.frame(
+  weighted.dens = seq(min(LATR_surv_exp_plot$mean_density),max(LATR_surv_exp_plot$mean_density),length.out = 20),
+  log_volume_t = LATR_surv_exp_plot$mean_size[1],
+  unique.transect=1,
+  transplant=T
+)
+LATR_surv_exp_pred$pred <- predict.gam(LATR_surv_best,newdata = LATR_surv_exp_pred, exclude = "s(unique.transect)")
+
+points(LATR_surv_exp_plot$mean_density,LATR_surv_exp_plot$mean_surv,ylim=c(0,1))
+lines(LATR_surv_exp_pred$weighted.dens,invlogit(LATR_surv_exp_pred$pred))
+
+
+# Recruitment -------------------------------------------------------------
+## estimate per-seed recruitment probability by estimating total seeds per window and total recruits per window
+
+LATR_recruits <- LATR_full %>% 
+  mutate(unique.transect = interaction(transect, site)) %>% 
+  group_by(year_t1,unique.transect,actual.window) %>% 
+  filter(seedling_t1==1) %>% 
+  summarise(recruits = n()) %>% 
+  rename(window=actual.window)
+
+## now estimate total seeds produced in each window using the known plant sizes and the fitted flowering and fruiting models
+LATR_transects <- read.csv("CData.Transects.Windows.csv") %>% 
+  mutate(unique.transect = interaction(transect, site),
+         log_volume_t = log(volume))
+LATR_transects$seeds = ceiling(invlogit(predict.gam(LATR_flower_best,newdata = LATR_transects)) * 
+                                 exp(predict.gam(LATR_fruits_best,newdata = LATR_transects))) 
+LATR_transects %>% 
+  group_by(unique.transect,window) %>% 
+  summarise(total_seeds=sum(seeds),
+            weighted.dens = unique(weighted.dens)) -> LATR_transects
+
+## now do something weird. take three copies of this df, assigning each one to a different year and assigning recruits to zero (for now)
+LATR_recruitment <- bind_rows(LATR_transects %>% filter(unique.transect=="1.FPS"|unique.transect=="2.FPS"|unique.transect=="3.FPS") %>% 
+                                mutate(year_t1=2014,recruits=0), ## only FPS for 2013-2014
+                              LATR_transects %>% mutate(year_t1=2015,recruits=0),
+                              LATR_transects %>% mutate(year_t1=2016,recruits=0),
+                              LATR_transects %>% mutate(year_t1=2017,recruits=0)) %>% 
+  left_join(.,LATR_recruits,by=c("year_t1","unique.transect","window")) %>% 
+  mutate(recruits.y=replace_na(recruits.y,0),
+         recruits = pmax(recruits.x,recruits.y,na.rm=T)) %>% 
+  drop_na()
+
+LATR_recruit <- list()
+LATR_recruit[[1]] <-  gam(cbind(recruits,total_seeds-recruits) ~ s(unique.transect,bs="re"),
+                          data=LATR_recruitment, gamma=1.4, family="binomial")
+LATR_recruit[[2]] <-  gam(cbind(recruits,total_seeds-recruits) ~ s(weighted.dens) + s(unique.transect,bs="re"),
+                          data=LATR_recruitment, gamma=1.4, family="binomial")
+recruit_aic<-AICtab(LATR_recruit,base=T,sort=F)
+LATR_recruit_best <- LATR_recruit[[which.min(recruit_aic$AIC)]]
+saveRDS(LATR_recruit_best,"LATR_recruit_best.rds")
+
+plot(LATR_recruitment$weighted.dens,LATR_recruitment$recruits/LATR_recruitment$total_seeds)
+LATR_recruitment$pred = predict.gam(LATR_recruit_best,newdata = LATR_recruitment,exclude="s(unique.transect)")
+points(LATR_recruitment$weighted.dens,invlogit(LATR_recruitment$pred),col="red",pch=".")
+
+
+# Seedlings size distribution ---------------------------------------------
+LATR_recruit_size <- LATR_full %>% 
+  filter(seedling_t1==1) %>% 
+  mutate(log_volume = log(volume_t1))
+
+hist(LATR_recruit_size$log_volume)
+saveRDS(data.frame(recruit_mean = mean(LATR_recruit_size$log_volume),
+                   recruit_sd = sd(LATR_recruit_size$log_volume)),"LATR_recruit_size.rds")
+
+
+# Size bounds -------------------------------------------------------------
+saveRDS(data.frame(min_size = log(min(LATR_full$volume_t,LATR_full$volume_t1[LATR_full$transplant==F],na.rm=T)),
+                   max_size = log(max(LATR_full$volume_t,LATR_full$volume_t1[LATR_full$transplant==F],na.rm=T))),
+        "LATR_size_bounds.rds")
