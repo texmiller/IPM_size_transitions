12c7b85e55150cd48f64fffe48d344b4dc8310c2
ellner <spe2@cornell.edu>
2022-08-12 19:18:17 -0400

    GSS strikes out

--- gam practice/qgam_kurtosis_practice.R
+++ gam practice/qgam_kurtosis_practice.R
@@ -17,10 +17,20 @@ source("../Diagnostics.R");
 ### Generate data from RSJP distribution
 ##  JP with mean=0, var=1, in (lambda, tau) parameters. 
 nx = 500; x = sort(2*rbeta(nx,3,3)); 
-taus = 0.5*(x-1); 
+taus = 0.75*(x-1); 
 y = matrix(NA,nx,10); 
 for(i in 1:nx) y[i,]=rRSJP(10,tau=taus[i])     
 xydata = data.frame(x = x, y = y); 
+
+#fit = kde2d(x,y[,1],n=20, h=c(0.2,1.5*bandwidth.nrd(y[,1])),lims=c(0,2,-4,4));
+#z2 = fit$z; 
+#for(i in 1:20){
+#    z2[i,]= dRSJP(fit$y,tau=0.5*(fit$x[i]-1)) * dbeta(fit$x[i]/2,3,3); 
+#}    
+#graphics.off(); 
+#dev.new(width=8,height=5); par(mfrow=c(1,2)); 
+#contour(fit$x,fit$y,fit$z/max(fit$z));
+#contour(fit$x,fit$y,z2/max(z2)); 
 
 ## Quantiles of the distribution 
 Y = matrix(NA,nx,5)
@@ -39,7 +49,7 @@ NPK = (Y[,5]- Y[,1])/(Y[,4]-Y[,2]);
 NPK = NPK/KG - 1; 
 
 dev.new(height=7,width=10); 
-par(mfcol=c(2,3),mar=c(4,4,2,1),mgp=c(2,1,0),bty="l"); 
+par(mfcol=c(2,4),mar=c(4,4,2,1),mgp=c(2,1,0),bty="l"); 
 
 # plot a typical data set, and true quantiles 
 plot(x,y[,1],xlab="Fitted",ylab="Residuals"); 
@@ -71,7 +81,7 @@ title(main=paste("RMSE=", RMSE));
 ################## REPEAT WITH SPLINES, df=1 to 3, by AIC  
 plot(x,y[,1],xlab="Fitted",ylab="Residuals"); 
 matpoints(x,Y, type="l",lty=2,lwd=2,col="red"); 
-title(main="B-spline with AIC 1.4"); 
+title(main="B-spline Q-regression, AIC 1.4"); 
 
 NPK_hat = matrix(NA,nx,10); 
 for(k in 1:10) {
@@ -110,3 +120,27 @@ matplot(x,cbind(NPK,NPK_hat),type="l", lty=c(1,rep(2,10)), col=c("black", rep("b
 err = scale(t(NPK_hat),center=NPK,scale=FALSE); RMSE = round(sqrt(mean(err^2)),digits=3); 
 rug(x); 
 title(main=paste("RMSE=", RMSE))
+
+
+################## REPEAT WITH SPLINE DENSITY ESTIMATE (gss package); 
+require(gss); 
+plot(x,y[,1],xlab="Fitted",ylab="Residuals"); 
+matpoints(x,Y, type="l",lty=2,lwd=2,col="red"); 
+title(main="GSS Spline Density Estimate"); 
+
+NPK_hat = matrix(NA,nx,10); 
+for(k in 1:10) {
+    z=y[,k]; X = data.frame(x=x,y=z); 
+    fit <- sscden1(~x + y + x*y, ~y,data=X)
+    quan <- qsscden(fit,c(.05,.25,.5,.75,.95),data.frame(x=X$x)); 
+    quan = t(quan); 
+    matpoints(x,quan, type="l",lty=1, lwd=1, col="grey50"); 
+    NPK_hat[,k] = ((quan[,5]-quan[,1])/(quan[,4]-quan[,2]))/KG - 1; 
+}
+
+matpoints(x,Y, type="l",lty=1,lwd=2,col="red"); 
+matplot(x,cbind(NPK,NPK_hat),type="l", lty=c(1,rep(2,10)), col=c("black", rep("blue",10)),lwd=c(2,rep(1,10))); 
+err = scale(t(NPK_hat),center=NPK,scale=FALSE); RMSE = round(sqrt(mean(err^2)),digits=3); 
+rug(x); 
+title(main=paste("RMSE=", RMSE))
+
