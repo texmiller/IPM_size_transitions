dc9ea545eef01db9fc412a6aa3d7d3f99a163891
ellner <spe2@cornell.edu>
2024-10-22 22:07:50 -0400

    more lichen bca

--- code/bca.R
+++ code/bca.R
@@ -1,5 +1,5 @@
 ############################################################################################# 
-## Code for BCa bootstrap confidence interval, copied from the "coxed" R package 
+## Code for BCa bootstrap confidence interval, based on code from the "coxed" R package 
 ## @Manual{,
 ##    title = {coxed: Duration-Based Quantities of Interest for the Cox Proportional Hazards Model},
 ##    author = {Kropko, Jonathan and Harden, {Jeffrey J.}},
@@ -8,16 +8,25 @@
 ##    url = {https://CRAN.R-project.org/package=coxed},
 ## }
 ## which in turn was adapted from code in the "mediation" R package. 
-##
 ## The "coxed" package was released with a GPL-2 license, which permits copying of the
 ## source code, as we have done here. Further use of the code, by yourself, is governed
-## by the terms of the GPL-2 license. 
+## by the terms of the GPL-2 license
 ##
-## This code implements the simplest form of the BCa interval, as described in 
+## This scripts corrects several important errors in the coxed and mediation routines.
+##  - the bias-correction factor z0 is correctly defined, using theta_hat instead of mean(theta)
+##  - the acceleration parameter a is an input parameter. The code in coxed and mediation
+##		calculates a from bootstrap parameter estimates, which is incorrect. The simplest
+##		approximation used jackknifed parameter estimates. 
+##
+## If given a valid estimate of a, this code implements the BCa interval as described in
 ##   Thomas J. DiCiccio and Bradley Efron (1996). Bootstrap Confidence Intervals. 
 ##   Statistical Science 11(3): 189–228. 
-##
-## It is exactly equivalent to bcanon() in the bootstrap package. 
+##   Bradley Efron and Robert J. Tibshirani (1994) An Introduction to the Bootstrap. 
+##	 Chapman & Hall/CRC, New York
+## 
+## If the acceleration parameter a is estimated from jackknifed parameter estimates
+## (eqn. (14.15), pg. 186 in Efron & Tibshirani) then it is exactly equivalent to
+## bcanon() in the bootstrap package. 
 ## @Manual{,
 ##    title = {bootstrap: Functions for the Book "An Introduction to the Bootstrap"},
 ##    author = {S original and from StatLib and by Rob Tibshirani. R port by Friedrich Leisch.},
@@ -25,17 +34,20 @@
 ##    note = {R package version 2019.6},
 ##    url = {https://CRAN.R-project.org/package=bootstrap},
 ## }
+##
+## With the default value a=0, the resulting intervals are bias-corrected by not "accelerated"
+## (i.e., there is no adjustment for skewness) and therefore not second-order accurate.  
 ################################################################################################
-bca = function (theta, theta_hat, conf.level = 0.95) {
+bca = function (theta, theta_hat, a=0, conf.level = 0.95) {
     low <- (1 - conf.level)/2
     high <- 1 - low
     sims <- length(theta)
     z.inv <- length(theta[theta < theta_hat])/sims
     z <- qnorm(z.inv)
-    U <- (sims - 1) * (mean(theta, na.rm = TRUE) - theta)
-    top <- sum(U^3)
-    under <- 6 * (sum(U^2))^{3/2}
-    a <- top/under
+    #U <- (sims - 1) * (mean(theta, na.rm = TRUE) - theta)
+    #top <- sum(U^3)
+    #under <- 6 * (sum(U^2))^{3/2}
+    #a <- top/under
     lower.inv <- pnorm(z + (z + qnorm(low))/(1 - a * (z + qnorm(low))))
     lower <- quantile(theta, lower.inv, names = FALSE)
     upper.inv <- pnorm(z + (z + qnorm(high))/(1 - a * (z + qnorm(high))))
--- lichen/Vulpicida_boot.R
+++ lichen/Vulpicida_boot.R
@@ -16,7 +16,7 @@ setwd(home); setwd("lichen");
 # require(tidyverse); require(maxLik); require(qgam); require(exactLTRE)
 
 require(mgcv); require(gamlss.dist); require(exactLTRE); 
-require(maxLik); require(doParallel); 
+require(maxLik); require(doParallel); require(moments); 
 
 source("../code/metaluck_fns_CMH.R"); 
 source("../code/bca.R"); 
@@ -86,19 +86,100 @@ mk_K_J <- function(m, L, U, L1, U1) {
   return(list(K = K, meshpts = meshpts, P = P, F = F))
 }
 
-
 ##################################################################
-##  Doing the bootstrap
+##  Read in the data 
 ##################################################################
-
-################# Read in data 
 XH_true = read.csv("Vulpicida raw data.csv"); 
 e = order(XH_true$t0); XH_true = XH_true[e,]; 
 
+##################################################################
+##  Doing the jacknife 
+##################################################################
 ncores = detectCores(logical=FALSE)-2; 
 c1 = makeCluster(ncores)
 registerDoParallel(c1); 
 clusterExport(c1,varlist=objects()); 
+
+jackreps = 1000 
+e_jack = sample(1:nrow(XH_true),jackreps, replace=FALSE); 
+clusterExport(c1,varlist=objects()); 
+
+################### START jackknife loop !!!!!!!!!!!!
+traits = foreach(jackrep = 1:jackreps,.packages = c("maxLik","gamlss.dist","mgcv"))%dopar%
+{ 
+
+omitted = e_jack[jackrep]; 
+XH = XH_true[-omitted,]; 
+e = order(XH$t0); XH = XH[e,]; 
+
+############### Survival modeling 
+fit2 = glm(survival~sqrt(t0) + t0,data=XH,family="binomial"); 
+surv_coefs = coef(fit2); 
+
+########################################################################
+#  Gaussian growth model: final choice  
+########################################################################
+XH = XH[XH$survival==1,]; ## discard those dead at (t+1); 
+fitGAU <- gam(list(t1~t0 + I(t0^2), ~t0 + I(t0^2)), data=XH, gamma=1.4,family=gaulss())
+
+################################################################# 
+## Make the GAUSSIAN IPM, compute traits and store 
+#################################################################
+L=-1; U=10; L1 = 0.2; U1 = 7; # limits of the data for eviction-prevention 
+
+IPM_G = mk_K_G(200,L=L,U=U,L1 = L1, U1 = U1); lambda = Re(eigen(IPM_G$K)$values[1]);
+matU = IPM_G$P; matF = IPM_G$F; 
+lichen_c0 = rep(0,nrow(matU)); lichen_c0[1]=1
+
+traits_G <-c(
+  lambda, 
+  mean_lifespan(matU, mixdist=lichen_c0),
+  mean_LRO(matU,matF,mixdist=lichen_c0),
+  mean_age_repro(matU,matF,mixdist=lichen_c0),
+  gen_time_mu1_v(matU,matF)
+)
+
+################################################################# 
+## Make the JSU IPM, compute traits and store 
+#################################################################
+
+p0<-c(coef(fitGAU)[1:6],0,0,0)
+## fit with maxlik
+outJSU=maxLik(logLik=LogLikJSU,start=p0*exp(0.2*rnorm(length(p0))), response=XH$t1,
+              method="BHHH",control=list(iterlim=5000,printLevel=0),finalHessian=FALSE); 
+outJSU=maxLik(logLik=LogLikJSU,start=outJSU$estimate,response=XH$t1,
+              method="NM",control=list(iterlim=5000,printLevel=0),finalHessian=FALSE); 
+outJSU=maxLik(logLik=LogLikJSU,start=outJSU$estimate,response=XH$t1,
+              method="BHHH",control=list(iterlim=5000,printLevel=0),finalHessian=FALSE); 
+
+IPM_J =mk_K_J(200,L=L,U=U,L1 = L1, U1 = U1); lambda = Re(eigen(IPM_J$K)$values[1]);
+
+### JSU
+matU = IPM_J$P; matF = IPM_J$F; c0 = rep(0,nrow(matU)); c0[1]=1; 
+traits_JSU<-c(
+  lambda, 
+  mean_lifespan(matU, mixdist=lichen_c0),
+  mean_LRO(matU,matF,mixdist=lichen_c0),
+  mean_age_repro(matU,matF,mixdist=lichen_c0),
+  gen_time_mu1_v(matU,matF)
+)
+
+c(traits_G,traits_JSU); 
+
+} 
+################### END jackknife loop !!!!!!!!!!!!
+
+#################################################################
+# Estimate acceleration parameter a from jackknifed estimates
+#################################################################
+theta = matrix(unlist(traits),ncol=10,byrow=TRUE); 
+a_G = apply(theta[,1:5],2, skewness)/6; 
+a_JSU = apply(theta[,6:10],2,skewness)/6; 
+
+
+##################################################################
+##  Doing the bootstrap
+##################################################################
 
 bootreps = 501; 
 ################### START bootstrap loop !!!!!!!!!!!!
@@ -186,7 +267,7 @@ xsd = apply(traits_G_boot,2,var)^0.5;
 ### Compute BCA intervals 
 CI_G = matrix(NA,2,5); 
 for(j in 1:5) {
-	CI_G[1:2,j]=bca(traits_G_boot[,j], conf.level = 0.95) 
+	CI_G[1:2,j]=bca(theta = traits_G_boot[,j], theta_hat = traits_G_true[j], a = a_G[j], conf.level = 0.95) 
 }
 
 cat("GAUSSIAN", "\n"); 
@@ -211,7 +292,7 @@ xsd = apply(traits_JSU_boot,2,var)^0.5;
 ### Compute BCA intervals 
 CI_J = matrix(NA,2,5); 
 for(j in 1:5) {
-	CI_J[1:2,j]=bca(traits_JSU_boot[,j], conf.level = 0.95) 
+	CI_J[1:2,j]=bca(theta  = traits_JSU_boot[,j], theta_hat = traits_JSU_true[j], a = a_JSU[j], conf.level = 0.95) 
 }
 
 cat("JSU", "\n");  
